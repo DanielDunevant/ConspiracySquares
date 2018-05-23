7695543541e36cac3c9797c5f29177b6be289757
Jesse T Primiani <jesseprimiani@gmail.com>
2018-05-23 15:33:58 -0500

    Update

--- app/src/main/java/com/novaytechnologies/conspiracysquares/Game_Draw.java
+++ app/src/main/java/com/novaytechnologies/conspiracysquares/Game_Draw.java
@@ -2,16 +2,32 @@
 
 package com.novaytechnologies.conspiracysquares;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
 public class Game_Draw extends FrameLayout {
+    Paint paint = new Paint();
 
     int nWidth;
     int nHeight;
+    int nWidth_Center;
+    int nHeight_Center;
+
+    int nBoxSize;
+
+    int nMoveX = 0;
+    int nMoveY = 0;
+
+    float fPlayerSpeed = 0f;
+    float fPlayerPosX = 0f;
+    float fPlayerPosY = 0f;
 
     public Game_Draw(Context ctx) {
         this(ctx, null);
@@ -32,9 +48,63 @@ public class Game_Draw extends FrameLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         nWidth = widthMeasureSpec;
         nHeight = heightMeasureSpec;
+
+        nWidth_Center = widthMeasureSpec/2;
+        nHeight_Center = heightMeasureSpec/2;
+
+        nBoxSize = (nWidth < nHeight) ? nWidth/20 : nHeight/20;
+        nBoxSize = (nBoxSize > 4) ? nBoxSize : 5;
+
+        fPlayerSpeed = nBoxSize * 20f;
     }
 
-    public void onDraw(Canvas canvas) {
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        nMoveX = (int)event.getX();
+        nMoveY = (int)event.getY();
+        if (event.getAction() == MotionEvent.ACTION_MOVE)
+            performClick();
+        return true;
+    }
+
+    @Override
+    public boolean performClick()
+    {
+        super.performClick();
+        //if (nMoveX > (nWidth_Center + nBoxSize) || nMoveX < (nWidth_Center - nBoxSize)
+        //        || nMoveY > (nHeight_Center + nBoxSize) || nMoveY < (nHeight_Center - nBoxSize))
+        //{
+            ValueAnimator MoveBox_X = ValueAnimator.ofFloat(fPlayerPosX, (float)nMoveX);
+            ValueAnimator MoveBox_Y = ValueAnimator.ofFloat(fPlayerPosY, (float)nMoveY);
+
+            int nDuration = 1000 * (int)(Math.sqrt((fPlayerPosX-nMoveX)*(fPlayerPosX-nMoveX) + (fPlayerPosY-nMoveY)*(fPlayerPosY-nMoveY)) / fPlayerSpeed);
+            MoveBox_X.setDuration(nDuration);
+            MoveBox_Y.setDuration(nDuration);
+
+            MoveBox_X.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    fPlayerPosX = (float)animation.getAnimatedValue();
+                }
+            });
+            MoveBox_Y.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    fPlayerPosY = (float)animation.getAnimatedValue();
+                }
+            });
+
+            MoveBox_X.start();
+            MoveBox_Y.start();
+        //}
+        return true;
+    }
+
+    public void onDraw(Canvas canvas)
+    {
         super.onDraw(canvas);
+
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(4);
+        canvas.drawRect(fPlayerPosX-nBoxSize, fPlayerPosY-nBoxSize, fPlayerPosX+nBoxSize, fPlayerPosY+nBoxSize, paint);
     }
 }
