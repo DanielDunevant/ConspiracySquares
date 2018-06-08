2d28147e3dcdf08ecc7c7a9e7ca4a5fe07fc581b
Jesse T Primiani <jesseprimiani@gmail.com>
2018-06-08 15:37:01 -0500

    Update

--- app/src/main/java/com/novaytechnologies/conspiracysquares/Layout_Main.java
+++ app/src/main/java/com/novaytechnologies/conspiracysquares/Layout_Main.java
@@ -8,6 +8,8 @@ import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,7 +45,15 @@ public class Layout_Main extends AppCompatActivity {
 
         final TextView txt_error = findViewById(R.id.ID_txt_error);
         final EditText input_name = findViewById(R.id.ID_input_name);
+
         input_name.setText(sm_strName);
+        input_name.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() > 1) sm_strName = charSequence.toString();
+            }
+            @Override public void afterTextChanged(Editable editable) {}
+        });
 
         final Button btn_play = findViewById(R.id.ID_btn_play);
         btn_play.setOnClickListener(
